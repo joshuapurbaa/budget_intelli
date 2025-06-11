362fed8377983c04da7daaabe0eb12670eec29d6
yoshuapurba <yoshua.purba@bankraya.co.id>
2025-06-11 15:04:46 +0700

    refactor: update localization imports in empty_home_content.dart

--- lib/features/home/view/widgets/empty_home_content.dart
+++ lib/features/home/view/widgets/empty_home_content.dart
@@ -1,10 +1,11 @@
 import 'package:budget_intelli/core/core.dart';
+import 'package:budget_intelli/core/l10n/app_localizations.dart';
 import 'package:budget_intelli/features/budget/budget_barrel.dart';
 import 'package:budget_intelli/features/home/home_barrel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
